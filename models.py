5ca9f2c7f93be5b7445b84c2f1be402836f6bc92
JKZuo <73583962+JKZuo@users.noreply.github.com>
2024-03-15 12:48:38 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,29 +1,25 @@
 from layers import *
-
+    
 class Model(nn.Module):
-    def __init__(self, t_dim, l_dim, u_dim, embed_dim, ex, num_heads=2, dropout=0.1):
+    def __init__(self, t_dimension, l_dimension, u_dimension, embedding_dimension, ex, num_heads=2, dropout_rate=0.1):
         super(Model, self).__init__()
-        emb_t = nn.Embedding(t_dim, embed_dim, padding_idx=0)
-        emb_l = nn.Embedding(l_dim, embed_dim, padding_idx=0)
-        emb_u = nn.Embedding(u_dim, embed_dim, padding_idx=0)
-        emb_su = nn.Embedding(2, embed_dim, padding_idx=0)
-        emb_sl = nn.Embedding(2, embed_dim, padding_idx=0)
-        emb_tu = nn.Embedding(2, embed_dim, padding_idx=0)
-        emb_tl = nn.Embedding(2, embed_dim, padding_idx=0)
-        embed_layers = emb_t, emb_l, emb_u, emb_su, emb_sl, emb_tu, emb_tl
+        emb_t = nn.Embedding(t_dimension, embedding_dimension, padding_idx=0)
+        emb_l = nn.Embedding(l_dimension, embedding_dimension, padding_idx=0)
+        emb_u = nn.Embedding(u_dimension, embedding_dimension, padding_idx=0)
+        emb_su = nn.Embedding(2, embedding_dimension, padding_idx=0)
+        emb_sl = nn.Embedding(2, embedding_dimension, padding_idx=0)
+        emb_tu = nn.Embedding(2, embedding_dimension, padding_idx=0)
+        emb_tl = nn.Embedding(2, embedding_dimension, padding_idx=0)
+        embedding_layers = emb_t, emb_l, emb_u, emb_su, emb_sl, emb_tu, emb_tl
 
-        self.MultiEmbed = MultiEmbed(ex, embed_dim, embed_layers)
+        self.MultiEmbed = CTRMultiEmbedding(ex, embedding_dimension, embedding_layers)
+        self.SelfAttention = SelfAttn_MultiHead(embedding_dimension, embedding_dimension, num_heads, dropout_rate)
+        self.Embedding = CTREmbedding(ex, embedding_dimension, l_dimension-1, embedding_layers)
+        self.Attention = CTRAttention(emb_l, l_dimension-1)
 
-        # self.SelfAttn = SelfAttn(embed_dim, embed_dim)  # initi
-        # self.SelfAttn = SelfAttn_dropout(embed_dim, embed_dim, dropout)
-        self.SelfAttn = SelfAttn_MultiHead(embed_dim, embed_dim, num_heads, dropout)
-
-        self.Embed = Embed(ex, embed_dim, l_dim-1, embed_layers)
-        self.Attn = Attn(emb_l, l_dim-1)
-
-    def forward(self, traj, mat1, mat2, vec, traj_len):
-        joint, delta = self.MultiEmbed(traj, mat1, traj_len)  
-        self_attn = self.SelfAttn(joint, delta, traj_len)
-        self_delta = self.Embed(traj[:, :, 1], mat2, vec, traj_len) 
-        output = self.Attn(self_attn, self_delta, traj_len)  
+    def forward(self, traj_input, mat1_input, mat2_input, vec_input, traj_length):
+        joint, delta = self.MultiEmbed(traj_input, mat1_input, traj_length)  
+        self_attn = self.SelfAttention(joint, delta, traj_length)
+        self_delta = self.Embedding(traj_input[:, :, 1], mat2_input, vec_input, traj_length) 
+        output = self.Attention(self_attn, self_delta, traj_length)  
         return output
